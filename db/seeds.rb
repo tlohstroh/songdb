341d9ab6bce4bb1eb2de0d39a2c86cb91a46619d
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-16 15:01:16 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.delete_all
+Artist.delete_all
+
+song1 = Song.create(song_name: "Sign of The Times")
+song2 = Song.create(song_name: "The Beautiful Ones")
+song3 = Song.create(song_name: "Housequake")
+
+song4 = Song.create(song_name: "As I sat sadly by her side")
+song5 = Song.create(song_name: "Darker with the day")
+song6 = Song.create(song_name: "And no more shall we part")
+
+song7 = Song.create(song_name: "Faraway")
+song8 = Song.create(song_name: "Identity Thief")
+song9 = Song.create(song_name: "March")
+
+prince = Artist.create(artist_name: "Prince")
+nick_cave_and_the_bad_seeds = Artist.create(artist_name: "Nick Cave and the Bad Seeds")
+joshua_redman = Artist.create(artist_name: "Joshua Redman")
+
+prince.songs << [song1]
+prince.songs << [song2]
+prince.songs << [song3]
+
+nick_cave_and_the_bad_seeds.songs << [song4]
+nick_cave_and_the_bad_seeds.songs << [song5]
+nick_cave_and_the_bad_seeds.songs << [song6]
+
+joshua_redman.songs << [song7]
+joshua_redman.songs << [song8]
+joshua_redman.songs << [song9]
