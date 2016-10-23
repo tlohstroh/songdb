45658820e185c20422054dc028d76446d5a04bc3
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-24 00:37:16 +0200

    Add songs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  # get 'songs/index'
+  #
+  # get 'songs/new'
+  #
+  # get 'songs/edit'
+  #
+  # get 'songs/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "artists#index"
 
 resources :artists
+resources :songs
 
 end
