8ae8dc6e21da162adcab7013297577ca8d691e5e
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-29 14:53:17 +0200

    Add CRUD controllers for songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,54 @@
 class SongsController < ApplicationController
   def index
-  end
-
-  def new
-  end
-
-  def edit
+    @songs  = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to edit_song_path, notice: "Song succesfully created"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+
+    if @song.update_attributes( song_params )
+      redirect_to edit_song_path(@song), notice: "Song succesfully updated"
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+
+    @song.destroy
+
+    redirect_to songs_path
+
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :artist)
+
+  end
+
 end
